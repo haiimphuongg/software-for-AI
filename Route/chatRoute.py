0943c42a89726020979893b7c2231ffe5ceb145d
haiimphuongg <111567342+haiimphuongg@users.noreply.github.com>
2024-06-27 18:49:52 +0700

    Update chatRoute.py

--- Route/chatRoute.py
+++ Route/chatRoute.py
@@ -1,23 +1,26 @@
 from typing import Any
+from langchain.llms import HuggingFaceHub
+from fastapi import APIRouter, Depends
 
-from fastapi import APIRouter
-from Utils.Utils import get_vector_store
+from Controller.authController import AuthController
+from Utils.Utils import get_vector_store, get_conversational_rag_chain, get_retriever
+
 
 chatRoute = APIRouter(tags=["ChatAPI"])
 
+
 @chatRoute.get("/")
-async def get_chat(query: str) -> Any:
-    vector_store = get_vector_store()
-    docs = vector_store.similarity_search_with_score(query, k=3)
+async def get_chat(
+        query: str = "",
+        decoded_token = Depends(AuthController())
+) -> Any:
+    session_id = decoded_token["id"]
+    conversational_rag_chain = get_conversational_rag_chain()
+    answer = conversational_rag_chain.invoke(
+        {"input": query},
+        config={
+            "configurable": {"session_id": session_id}
+        },  # constructs a key "abc123" in `store`.
+    )["answer"]
 
-    data_return = []
-    for doc in docs:
-        data_return.append([str(doc[0].page_content), doc[1]])
-    return data_return
-
-
-
-
-
-
-
+    return answer
